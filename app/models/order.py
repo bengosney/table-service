f9b1102eca572fa638f6e32fdd38d22ce528f627
Ben Gosney <bengosney@googlemail.com>
2021-06-16 13:48:52 +0100

    order relationship

--- app/models/order.py
+++ app/models/order.py
@@ -1,6 +1,23 @@
+from sqlalchemy import Column
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import ForeignKey, Table
+from sqlalchemy.sql.sqltypes import Integer
+
 from app.database import Base
 from app.models.mixins import BaseClass
 
+order_link_product = Table(
+    "order_link_product",
+    Base.metadata,
+    Column("order_id", Integer, ForeignKey("orders.id")),
+    Column("product_id", Integer, ForeignKey("products.id")),
+)
+
 
 class Order(Base, BaseClass):
-    pass
+    table_id = Column(Integer, ForeignKey("tables.id"))
+    table = relationship(
+        "Table",
+    )
+
+    products = relationship("Product", secondary=order_link_product)
