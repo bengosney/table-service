29ad609085da6d9a8ee4214bebfc98346566fc11
Ben Gosney <bengosney@googlemail.com>
2021-06-21 14:43:34 +0100

    Add some layout, that doesn't work

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,73 +2,71 @@ import Form from "@rjsf/fluent-ui";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import useGroupedSchemas from "./hooks/useGroupedSchemas";
 import useSettings from "./hooks/useSettings";
-import Nav from './components/nav';
-import Admin from './components/admin';
+import Nav from "./components/nav";
+import Admin from "./components/admin";
 import { ucfirst } from "./utils";
 
 import "./App.css";
-
 
 function App() {
   const settings = useSettings();
   const grouped_schemas = useGroupedSchemas(settings.apiURL);
 
-  const navItems = Object.keys(grouped_schemas).filter((key) => key.toLowerCase() != 'order').map((key) => {
-    const url = `/admin/${key.toLowerCase()}`;
-    return {
-      name: ucfirst(key),
-      url: url,
-    };
-  });
+  const navItems = Object.keys(grouped_schemas)
+    .filter(key => key.toLowerCase() != "order")
+    .map(key => {
+      const url = `/admin/${key.toLowerCase()}`;
+      return {
+        name: ucfirst(key),
+        url: url
+      };
+    });
 
   const nav = [
     {
       links: [
         {
           name: "Home",
-          url: "/",
+          url: "/"
         },
         {
           name: "Orders",
-          url: "/orders/",
+          url: "/orders/"
         },
         {
           name: "Admin",
           url: "/admin/",
-          links: navItems,
-        },
-      ],
-    },
+          links: navItems
+        }
+      ]
+    }
   ];
   const schemas = {};
 
   return (
     <div className="App">
-      <Router>
-        <div>
-          <Nav groups={nav}  />
-        </div>
-        <div>
-          {Object.keys(schemas).map((k) => (
-            <div key={k}>
-              <p>{k}</p>
-              <Form schema={schemas[k]} />
-              <hr />
+      <div className={"ms-Grid"}>
+        <div className={"ms-Grid-row "}>
+          <Router>
+            <div className={"ms-Grid-col ms-md4"}>
+              <Nav groups={nav} />
             </div>
-          ))}
+            <div className={"ms-Grid-col ms-md6"}>
+              <Switch>
+                <Route path="/" exact>
+                  <h1>Home</h1>
+                </Route>
+                <Route path="/orders/">
+                  <h1>Orders</h1>
+                </Route>
+                <Route path="/admin/">
+                  <Admin />
+                </Route>
+              </Switch>
+            </div>
+          </Router>
         </div>
-        <Switch>
-          <Route path="/" exact>
-            <h1>Home</h1>
-          </Route>
-          <Route path="/orders/">
-            <h1>Orders</h1>
-          </Route>
-          <Route path="/admin/">
-            <Admin />
-          </Route>
-        </Switch>
-      </Router>
+      </div>
     </div>
   );
 }
