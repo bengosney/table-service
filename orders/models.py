83ae063ce4d0f483b808d96c8f17f9a793e50df0
Ben Gosney <bengosney@googlemail.com>
2021-06-22 21:55:07 +0100

    Better models

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,12 @@
+# Standard Library
+from enum import Enum, auto
+
 # Django
 from django.db import models
 from django.utils.translation import gettext as _
+
+# Third Party
+from django_fsm import FSMField, transition
 
 # First Party
 from tableservice.modelMixins import BaseModel, Info, NoDelete
@@ -17,12 +23,47 @@ class Product(BaseModel):
     name = models.CharField(_("Name"), max_length=255)
     in_stock = models.BooleanField(_("In Stock"), default=True)
     categories = models.ManyToManyField(Category)
+    price = models.FloatField(_("Price"), default=0)
 
 
 class Table(BaseModel):
     pass
 
 
+class OrderStates(Enum):
+    def _generate_next_value_(name, start, count, last_values):
+        return name
+
+    Unprocessed = auto()
+    Processing = auto()
+    Processed = auto()
+
+    @classmethod
+    def choices(cls):
+        return [[name, name] for name, _ in cls.__members__.items()]
+
+    @classmethod
+    def maxLength(cls):
+        return max(len(item.value) for _, item in cls.__members__.items())
+
+
 class Order(NoDelete, Info):
+
     table = models.ForeignKey(Table, on_delete=models.PROTECT)
     products = models.ManyToManyField(Product)
+
+    state = FSMField(
+        _("State"),
+        default=OrderStates.Unprocessed,
+        choices=OrderStates.choices(),
+        protected=True,
+        max_length=OrderStates.maxLength(),
+    )
+
+    @transition(field=state, source=OrderStates.Unprocessed, target=OrderStates.Processing)
+    def processing(self):
+        pass
+
+    @transition(field=state, source=[OrderStates.Unprocessed, OrderStates.Processing], target=OrderStates.Processed)
+    def processed(self):
+        pass
